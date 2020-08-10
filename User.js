fdc4b6b3a296b9952e2fc74071928286d2b9a0e5
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-08-10 18:27:34 +0200

    add connection string

--- User.js
+++ User.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
