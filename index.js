a66f5f3da708934322b1cecbe8d34628f3536848
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-08-10 16:33:31 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const DBL = require("dblapi.js");
+
+const token = process.env.TOPGG_TOKEN
+const port = process.env.TOPGG_PORT
+const auth = process.env.TOPGG_AUTH
+
+const dbl = new DBL(token, { webhookPort: port, webhookAuth: auth });
+
+dbl.webhook.on('ready', hook => {
+  console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+});
+dbl.webhook.on('vote', vote => {
+  console.log(`User with ID ${vote.user} just voted!`);
+});
+// Optional events
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+})
+
+dbl.on('error', e => {
+ console.log(`Oops! ${e}`);
+})
