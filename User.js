a693e286a6bfe2e6b4111eaa019740d8c99fcae5
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-08-12 18:05:01 +0200

    add default values

--- User.js
+++ User.js
@@ -6,12 +6,54 @@ mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlP
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  cooldowns:{
+    miniboss:{
+		type:Date,
+		default: 0,
+	},
+  dungeon:{
+		type:Date,
+		default: 0,
+	},
+  },
     hero: {
 		inventory:{
-            Carrot:{
-                type: Number,
-                default: 0,
-            }
+      ["Small Healing Potion"]: {
+        type: Number,
+        default: 5,
+      },
+      ["Large Healing Potion"]: {
+        type: Number,
+        default: 0,
+      },
+      ["Enourmous Healing Potion"]: {
+        type: Number,
+        default: 0,
+      },
+      ["Quality Healing Potion"]: {
+        type: Number,
+        default: 0,
+      },
+      ["Mega Healing Potion"]: {
+        type: Number,
+        default: 0,
+      },
+      ["Ultra Healing Potion"]: {
+        type: Number,
+        default: 0,
+      },
+      ["Small Healing Salve"]: {
+        type: Number,
+        default: 0,
+      },
+      ["Large Healing Salve"]: {
+        type: Number,
+        default: 0,
+      },
+      Carrot:{
+          type: Number,
+          default: 0,
+      }
 		},
     }
 }, { strict: false });
